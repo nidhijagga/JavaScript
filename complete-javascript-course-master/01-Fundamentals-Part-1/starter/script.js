8b0e49b50d37a17251fa98fc167869baac9ede3e
nidhijagga <nidhijagga5239@gmail.com>
2023-01-22 21:43:20 +0530

    5 Variables and Values

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+let js = "amazing";
+if(js === 'amazing'){
+  alert('JS is Fun');
+}
+
+//When we are in browser console This will run 89+56
+89+56; //This will not run in browser console when we open the code in live server
+console.log(89+57); // This will work fine, as now we are not in console itself we are in the script file, where we have to mention them that we need this value in console.
+
+
+
+let a = 67;
+//a : variable
+//67: value
+
+console.log(a);
+
+//Naming variables
+
+//1. Camel Case
+let camelCase;
+
+//2. Cant start with numbers
+// let 3years;
+
+//3. Only we can use numbers, letter, underscore and $ 
+let nidhi1;
+let nidhi_jagga;
+let nidhi$;
+
+//4. cant use reserved JS keywords
+//eg new, function
+
+//5. We should not start with upperCase.
+
+//6.Should be descriptive.
