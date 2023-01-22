f258a2d3245239d89e8db4c53574c7ae7ea42bca
nidhijagga <nidhijagga5239@gmail.com>
2023-01-22 23:31:08 +0530

    15 Operator Precedence

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -85,6 +85,7 @@ console.log(firstName + " " + lastName);
 */
 
 
+/*
 
 ////////////////////////////////////
 // Basic Operators
@@ -100,7 +101,6 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 const firstName = 'Jonas';
 const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
-
 // Assignment operators
 let x = 10 + 5; // 15
 x += 10; // x = x + 10 = 25
@@ -117,4 +117,21 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
 
+
+
+////////////////////////////////////
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
