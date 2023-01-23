e7243f4bfcb1264efe59ca652786e2f05db5a852
nidhijagga <nidhijagga5239@gmail.com>
2023-01-23 21:56:47 +0530

    37 Reviewing Functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -74,16 +74,38 @@
 
 ///////////////////////////////////////
 // Functions Calling Other Functions
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
 
-    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
-    return juice;
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
 }
-console.log(fruitProcessor(2, 3));
 
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
