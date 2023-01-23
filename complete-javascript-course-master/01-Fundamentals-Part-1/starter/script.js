ac2455c0c53cb68bbcc7cd551b800f3cb5c26e44
nidhijagga <nidhijagga5239@gmail.com>
2023-01-23 20:35:45 +0530

    26 The Switch Statement

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -378,29 +378,74 @@ GOOD LUCK 😀
 */
 
 
-// const scoreDolphins = (96 + 108 + 89) / 3;
-// const scoreKoalas = (88 + 91 + 110) / 3;
+// // const scoreDolphins = (96 + 108 + 89) / 3;
+// // const scoreKoalas = (88 + 91 + 110) / 3;
+// // console.log(scoreDolphins, scoreKoalas);
+
+// // if (scoreDolphins > scoreKoalas) {
+// //   console.log('Dolphins win the trophy 🏆');
+// // } else if (scoreKoalas > scoreDolphins) {
+// //   console.log('Koalas win the trophy 🏆');
+// // } else if (scoreDolphins === scoreKoalas) {
+// //   console.log('Both win the trophy!');
+// // }
+
+// // BONUS 1
+// const scoreDolphins = (97 + 112 + 80) / 3;
+// const scoreKoalas = (109 + 95 + 50) / 3;
 // console.log(scoreDolphins, scoreKoalas);
 
-// if (scoreDolphins > scoreKoalas) {
-//   console.log('Dolphins win the trophy 🏆');
-// } else if (scoreKoalas > scoreDolphins) {
-//   console.log('Koalas win the trophy 🏆');
-// } else if (scoreDolphins === scoreKoalas) {
-//   console.log('Both win the trophy!');
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//     console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//     console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+//     console.log('Both win the trophy!');
+// } else {
+//     console.log('No one wins the trophy 😭');
 // }
 
-// BONUS 1
-const scoreDolphins = (97 + 112 + 80) / 3;
-const scoreKoalas = (109 + 95 + 50) / 3;
-console.log(scoreDolphins, scoreKoalas);
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-    console.log('Dolphins win the trophy 🏆');
-} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
-    console.log('Koalas win the trophy 🏆');
-} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
-    console.log('Both win the trophy!');
+
+
+////////////////////////////////////
+// The switch Statement
+const day = 'friday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
 } else {
-    console.log('No one wins the trophy 😭');
+    console.log('Not a valid day!');
 }
