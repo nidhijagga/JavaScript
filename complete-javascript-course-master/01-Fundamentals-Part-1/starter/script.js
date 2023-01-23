82b24a30265e65a81387e752d512dccc414edb96
nidhijagga <nidhijagga5239@gmail.com>
2023-01-23 20:40:59 +0530

    28 Ternary Operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -453,14 +453,33 @@ GOOD LUCK 😀
 
 
 ////////////////////////////////////
-// Statements and Expressions
-3 + 4
-1991
-true && false && !false
+// // Statements and Expressions
+// 3 + 4
+// 1991
+// true && false && !false
 
-if (23 > 10) {
-    const str = '23 is bigger';
+// if (23 > 10) {
+//     const str = '23 is bigger';
+// }
+
+// const me = 'Jonas';
+// console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine 🍷';
+} else {
+    drink2 = 'water 💧';
 }
+console.log(drink2);
 
-const me = 'Jonas';
-console.log(`I'm ${2037 - 1991} years old ${me}`);
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
