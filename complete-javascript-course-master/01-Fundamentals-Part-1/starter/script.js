a1dc62165b04e82f2ae74fefcfdf49ce9713de4c
nidhijagga <nidhijagga5239@gmail.com>
2023-01-23 20:13:33 +0530

    22 The Equality Operator == And ===

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -283,26 +283,51 @@ if (BMIMark > BMIJohn) {
 // n = n - 1;//10
 // console.log(n);
 
+// ////////////////////////////////////
+// // Truthy and Falsy Values
+
+// // 5 falsy values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+// if (money) {
+//     console.log("Don't spend it all ;)");
+// } else {
+//     console.log('You should get a job!');
+// }
+
+// let height;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED');
+// }
+
+
 ////////////////////////////////////
-// Truthy and Falsy Values
+// Equality Operators: == vs. ===
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
 
-// 5 falsy values: 0, '', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
+if (age == 18) console.log('You just became an adult :D (loose)');
 
-const money = 0;
-if (money) {
-    console.log("Don't spend it all ;)");
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // 23 === 23 -> FALSE
+    console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
 } else {
-    console.log('You should get a job!');
+    console.log('Number is not 23 or 7 or 9')
 }
 
-let height;
-if (height) {
-    console.log('YAY! Height is defined');
-} else {
-    console.log('Height is UNDEFINED');
-}
+if (favourite !== 23) console.log('Why not 23?');
+
