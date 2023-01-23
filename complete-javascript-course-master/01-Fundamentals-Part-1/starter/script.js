abdfd8323658f0e98fca573392ba9184c9c007d7
nidhijagga <nidhijagga5239@gmail.com>
2023-01-23 19:58:49 +0530

    21 Truthy and Falsy Values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -260,25 +260,49 @@ if (BMIMark > BMIJohn) {
 */
 
 
+// ////////////////////////////////////
+// // Type Conversion and Coercion
+
+// // type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));
+// //NaN - Not a number.
+// console.log(typeof NaN); //number
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);//10
+// console.log('23' / '2');//11.5
+
+// let n = '1' + 1; // '11'
+// n = n - 1;//10
+// console.log(n);
+
 ////////////////////////////////////
-// Type Conversion and Coercion
+// Truthy and Falsy Values
 
-// type conversion
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
 
-console.log(Number('Jonas'));
-//NaN - Not a number.
-console.log(typeof NaN); //number 
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
 
-console.log(String(23), 23);
-
-// type coercion
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);//10
-console.log('23' / '2');//11.5
-
-let n = '1' + 1; // '11'
-n = n - 1;//10
-console.log(n);
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
